5a8a0011c39af9dd6b3d13b6b0839750ee7f1538
Annisa Ainin Fadlilah <92904284+annisaainin@users.noreply.github.com>
2022-07-12 18:58:32 +0700

    Update post.blade.php

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -22,7 +22,6 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav">
-            <a class="nav-link active" aria-current="page" href="https://github.com/rafiadw/integratif-it0202-1202190040"></a>
           </div>
         </div>
       </div>
